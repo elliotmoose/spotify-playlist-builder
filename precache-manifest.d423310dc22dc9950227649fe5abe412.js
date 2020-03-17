8124ad8367b1edaa1b229f005cfe73df4d4866bf
Elliot Koh <appdev.moose@gmail.com>
2020-03-17 15:47:19 +0800

    first commit

---
+++ precache-manifest.d423310dc22dc9950227649fe5abe412.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f6ea09ed2208d820e98e55e62c28732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d94baf3b2bd398334a6",
+    "url": "/static/css/main.ed04d66d.chunk.css"
+  },
+  {
+    "revision": "be32c5199ddb7e3ce7eb",
+    "url": "/static/js/2.28571250.chunk.js"
+  },
+  {
+    "revision": "7c8ccf8d8cd6157a1e8ac24e995d0ce8",
+    "url": "/static/js/2.28571250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d94baf3b2bd398334a6",
+    "url": "/static/js/main.20d12de5.chunk.js"
+  },
+  {
+    "revision": "76e89bab765fbfcf2793",
+    "url": "/static/js/runtime-main.4b420bf7.js"
+  },
+  {
+    "revision": "d3085f686df272f9e1a267cc69b2d24f",
+    "url": "/static/media/Montserrat-Bold.d3085f68.ttf"
+  },
+  {
+    "revision": "07689d4eaaa3d530d58826b5d7f84735",
+    "url": "/static/media/Montserrat-Regular.07689d4e.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
